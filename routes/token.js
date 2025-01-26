1abdf358efbaf7b1101778ae0c4a143914d6e390
G7cars <g7selfdrivecars@gmail.com>
2025-01-27 00:25:13 +0530

    a

--- routes/token.js
+++ routes/token.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Initialize DynamoDB document client
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
+const table = "UserTokens";
 // Route to save user token
 router.post('/register-token', async (req, res) => {
   const { pushToken, subscriptionId } = req.body; // Extract token and subscription ID from request body
@@ -14,20 +14,21 @@ router.post('/register-token', async (req, res) => {
     return res.status(400).json({ message: 'pushToken and subscriptionId are required.' });
   }
 
-  const params = {
-    TableName: 'UserTokens', // Your DynamoDB table name
-    Item: {
-      partitionKey: 'Qnews', // Fixed partition key
-      subscriptionId, // Unique identifier for the subscription
-      pushToken, // The device token (OneSignal ID)
-      createdAt: new Date().toISOString(), // Timestamp
-    },
-    ConditionExpression: 'attribute_not_exists(subscriptionId)', // Prevent duplicate tokens
-  };
-
+  const item = {
+    pushToken,
+    subscriptionId,
+    createdAt: new Date().toISOString(),
+  }
+  
   try {
     // Store the token in DynamoDB
-    await dynamoDB.put(params).promise();
+    await dynamoDB.put(
+      {
+        TableName: table,
+        Item: item,
+        ConditionExpression: 'attribute_not_exists(subscriptionId)', 
+      }
+    ).promise();
     res.status(200).json({ message: 'Token saved successfully.' });
   } catch (error) {
     if (error.code === 'ConditionalCheckFailedException') {
@@ -39,24 +40,5 @@ router.post('/register-token', async (req, res) => {
   }
 });
 
-// Route to fetch all tokens (for sending notifications)
-router.get('/get-tokens', async (req, res) => {
-  const params = {
-    TableName: 'UserTokens', // Your DynamoDB table name
-    KeyConditionExpression: 'partitionKey = :partitionKey', // Query by partition key
-    ExpressionAttributeValues: {
-      ':partitionKey': 'Qnews', // Fixed partition key
-    },
-  };
-
-  try {
-    const result = await dynamoDB.query(params).promise();
-    const tokens = result.Items.map((item) => item.pushToken); // Extract only the tokens
-    res.status(200).json({ tokens });
-  } catch (error) {
-    console.error('Error fetching tokens:', error);
-    res.status(500).json({ message: 'Error fetching tokens.', error });
-  }
-});
 
 module.exports = router;
